8bad4256301496fb340e057ab337cb5c29345e12
ZoltanSerdult <91883365+ZoltanSerdult@users.noreply.github.com>
2022-04-14 23:02:21 +0200

    Add files via upload

--- vizsgaremek.sql
+++ vizsgaremek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.4
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Feb 09, 2022 at 09:40 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.3.27
+-- Gép: 127.0.0.1
+-- Létrehozás ideje: 2022. Már 30. 18:56
+-- Kiszolgáló verziója: 10.4.19-MariaDB
+-- PHP verzió: 8.0.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `vizsgaremek`
+-- Adatbázis: `vizsgaremek`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `beerkezesek`
+-- Tábla szerkezet ehhez a táblához `beerkezesek`
 --
 
 CREATE TABLE `beerkezesek` (
@@ -37,7 +37,7 @@ CREATE TABLE `beerkezesek` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `beerkezett_termek`
+-- Tábla szerkezet ehhez a táblához `beerkezett_termek`
 --
 
 CREATE TABLE `beerkezett_termek` (
@@ -49,7 +49,7 @@ CREATE TABLE `beerkezett_termek` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `dolgozo`
+-- Tábla szerkezet ehhez a táblához `dolgozo`
 --
 
 CREATE TABLE `dolgozo` (
@@ -59,10 +59,17 @@ CREATE TABLE `dolgozo` (
   `nev` varchar(50) COLLATE utf8_hungarian_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- A tábla adatainak kiíratása `dolgozo`
+--
+
+INSERT INTO `dolgozo` (`dolgozo_id`, `felhasznalonev`, `jelszo`, `nev`) VALUES
+(1, 'Raktaros1', '1234', 'Serdült Zoltán');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rendeles`
+-- Tábla szerkezet ehhez a táblához `rendeles`
 --
 
 CREATE TABLE `rendeles` (
@@ -76,7 +83,7 @@ CREATE TABLE `rendeles` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rendelt_termek`
+-- Tábla szerkezet ehhez a táblához `rendelt_termek`
 --
 
 CREATE TABLE `rendelt_termek` (
@@ -89,7 +96,7 @@ CREATE TABLE `rendelt_termek` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `termek`
+-- Tábla szerkezet ehhez a táblához `termek`
 --
 
 CREATE TABLE `termek` (
@@ -111,7 +118,7 @@ CREATE TABLE `termek` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
--- Dumping data for table `termek`
+-- A tábla adatainak kiíratása `termek`
 --
 
 INSERT INTO `termek` (`termek_id`, `nev`, `ar`, `keszlet`, `foglalat`, `forma`, `teljesitmeny`, `szinhomerseklet`, `termekkod`, `ean`, `allapot`, `leiras`, `elerheto`, `akcio`, `marka`) VALUES
@@ -119,12 +126,15 @@ INSERT INTO `termek` (`termek_id`, `nev`, `ar`, `keszlet`, `foglalat`, `forma`, 
 (3, 'Bright Stick T37 8W E14 4000K', 990, 0, 'e14', 't37', 8, 4000, 'bse148w4000', 2, 'épp', 'Az Avide LED-fényforrások a hagyományos izzókat helyettesítő magas hatékonyságú LED-ek. Tökéletesek háztartásokban található világítástechnikai megoldások lecserélésére. Többféle formában érhetők el, mint például különböző méretű gömb, mini gömb, stick, gyertya, szpot, reflektív és kapszula. Minden típusú égőnk megtalálható többféle színhőmérsékletben, több különböző foglalattal. Alacsony fogyasztásuknak és hosszú élettartamuknak köszönhetően jelentős megtakarítást eredményeznek. Nem villognak, kímélik a szemet. A ki- és bekapcsolásnak nincs élettartamot rövidítő hatása.\r\n\r\nA LED fényforrások nem egy fémszál felmelegedéséből érik el a fény kibocsátását, hanem elektronok szabadulnak fel, ezáltal minimális a hőveszteségük. LED technológiával a hagyományos izzókhoz képest akár 80%-os energiamegtakarítás is elérhető. A LED fényforrások hőtermelése minimális, ezáltal felhasználható olyan helyeken is, ahol a melegedés veszélyforrás lehet.', 1, 0, ''),
 (4, 'Candle C35 4W E14 4000K', 990, 0, 'e14', 'c35', 4, 4000, 'ce144w4000', 3, 'épp', 'Az Avide LED-fényforrások a hagyományos izzókat helyettesítő magas hatékonyságú LED-ek. Tökéletesek háztartásokban található világítástechnikai megoldások lecserélésére. Többféle formában érhetők el, mint például különböző méretű gömb, mini gömb, stick, gyertya, szpot, reflektív és kapszula. Minden típusú égőnk megtalálható többféle színhőmérsékletben, több különböző foglalattal. Alacsony fogyasztásuknak és hosszú élettartamuknak köszönhetően jelentős megtakarítást eredményeznek. Nem villognak, kímélik a szemet. A ki- és bekapcsolásnak nincs élettartamot rövidítő hatása.\r\n\r\nA LED fényforrások nem egy fémszál felmelegedéséből érik el a fény kibocsátását, hanem elektronok szabadulnak fel, ezáltal minimális a hőveszteségük. LED technológiával a hagyományos izzókhoz képest akár 80%-os energiamegtakarítás is elérhető. A LED fényforrások hőtermelése minimális, ezáltal felhasználható olyan helyeken is, ahol a melegedés veszélyforrás lehet.', 1, 0, ''),
 (5, 'Candle C35 4W E14 3000K', 990, 0, 'e14', 'c35', 4, 3000, 'ce144w3000', 4, 'épp', 'Az Avide LED-fényforrások a hagyományos izzókat helyettesítő magas hatékonyságú LED-ek. Tökéletesek háztartásokban található világítástechnikai megoldások lecserélésére. Többféle formában érhetők el, mint például különböző méretű gömb, mini gömb, stick, gyertya, szpot, reflektív és kapszula. Minden típusú égőnk megtalálható többféle színhőmérsékletben, több különböző foglalattal. Alacsony fogyasztásuknak és hosszú élettartamuknak köszönhetően jelentős megtakarítást eredményeznek. Nem villognak, kímélik a szemet. A ki- és bekapcsolásnak nincs élettartamot rövidítő hatása.\r\n\r\nA LED fényforrások nem egy fémszál felmelegedéséből érik el a fény kibocsátását, hanem elektronok szabadulnak fel, ezáltal minimális a hőveszteségük. LED technológiával a hagyományos izzókhoz képest akár 80%-os energiamegtakarítás is elérhető. A LED fényforrások hőtermelése minimális, ezáltal felhasználható olyan helyeken is, ahol a melegedés veszélyforrás lehet.', 1, 0, ''),
-(6, 'Globe A60 8W E27 4000K', 890, 0, 'e27', 'a60', 8, 4000, 'ge278w4000', 5, 'épp', 'Az Avide LED-fényforrások a hagyományos izzókat helyettesítő magas hatékonyságú LED-ek. Tökéletesek háztartásokban található világítástechnikai megoldások lecserélésére. Többféle formában érhetők el, mint például különböző méretű gömb, mini gömb, stick, gyertya, szpot, reflektív és kapszula. Minden típusú égőnk megtalálható többféle színhőmérsékletben, több különböző foglalattal. Alacsony fogyasztásuknak és hosszú élettartamuknak köszönhetően jelentős megtakarítást eredményeznek. Nem villognak, kímélik a szemet. A ki- és bekapcsolásnak nincs élettartamot rövidítő hatása.\r\n\r\nA LED fényforrások nem egy fémszál felmelegedéséből érik el a fény kibocsátását, hanem elektronok szabadulnak fel, ezáltal minimális a hőveszteségük. LED technológiával a hagyományos izzókhoz képest akár 80%-os energiamegtakarítás is elérhető. A LED fényforrások hőtermelése minimális, ezáltal felhasználható olyan helyeken is, ahol a melegedés veszélyforrás lehet.', 1, 0, '');
+(6, 'Globe A60 8W E27 4000K', 890, 0, 'e27', 'a60', 8, 4000, 'ge278w4000', 5, 'épp', 'Az Avide LED-fényforrások a hagyományos izzókat helyettesítő magas hatékonyságú LED-ek. Tökéletesek háztartásokban található világítástechnikai megoldások lecserélésére. Többféle formában érhetők el, mint például különböző méretű gömb, mini gömb, stick, gyertya, szpot, reflektív és kapszula. Minden típusú égőnk megtalálható többféle színhőmérsékletben, több különböző foglalattal. Alacsony fogyasztásuknak és hosszú élettartamuknak köszönhetően jelentős megtakarítást eredményeznek. Nem villognak, kímélik a szemet. A ki- és bekapcsolásnak nincs élettartamot rövidítő hatása.\r\n\r\nA LED fényforrások nem egy fémszál felmelegedéséből érik el a fény kibocsátását, hanem elektronok szabadulnak fel, ezáltal minimális a hőveszteségük. LED technológiával a hagyományos izzókhoz képest akár 80%-os energiamegtakarítás is elérhető. A LED fényforrások hőtermelése minimális, ezáltal felhasználható olyan helyeken is, ahol a melegedés veszélyforrás lehet.', 1, 0, ''),
+(7, 'fénycsö', 1000, 20, 'Gömb', 'Ovális', 120, 12, 'DEB214', 434312321, 'Épp', 'Lurem ipsum', 1, 20, 'Lux'),
+(8, '30 cm-es T5 LED fénycső armatúrával', 2500, 2, 'T5', '', 123, 2500, 'DEB001', 658697, 'Épp', 'Cseréld le áramzabáló világításodat erre a praktikus, energiatakarékos fénycsőre! A 30 cm-es armatúra bárhova felszerelhető, így akár a mosogatót vagy a fürdőszobai tükröt is tökéletesen megvilágíthatod. Az armatúrán találsz egy kapcsolót, a mellékelt villásdugó pedig még egyszerűbbé teszi beüzemelését. A T5-ös fénycső 4W fogyasztás mellett kb. 40W-os fényerőt biztosít.', 2, 5, 'Outor'),
+(9, 'Minuszos', 2000, -5, 'Nincs', 'Formás', 100, 100, 'DEB001-23', 223423, '2', 'nincs', 0, 20, 'Valami');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `vasarlo`
+-- Tábla szerkezet ehhez a táblához `vasarlo`
 --
 
 CREATE TABLE `vasarlo` (
@@ -142,114 +152,114 @@ CREATE TABLE `vasarlo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
--- Indexes for dumped tables
+-- Indexek a kiírt táblákhoz
 --
 
 --
--- Indexes for table `beerkezesek`
+-- A tábla indexei `beerkezesek`
 --
 ALTER TABLE `beerkezesek`
   ADD PRIMARY KEY (`beerk_id`),
   ADD KEY `dolgozo` (`dolgozo_id`);
 
 --
--- Indexes for table `beerkezett_termek`
+-- A tábla indexei `beerkezett_termek`
 --
 ALTER TABLE `beerkezett_termek`
   ADD KEY `beerkezes` (`beerk_id`),
   ADD KEY `termek` (`termek_id`);
 
 --
--- Indexes for table `dolgozo`
+-- A tábla indexei `dolgozo`
 --
 ALTER TABLE `dolgozo`
   ADD PRIMARY KEY (`dolgozo_id`);
 
 --
--- Indexes for table `rendeles`
+-- A tábla indexei `rendeles`
 --
 ALTER TABLE `rendeles`
   ADD PRIMARY KEY (`rendeles_id`),
   ADD KEY `vasarloo` (`vasarlo_id`);
 
 --
--- Indexes for table `rendelt_termek`
+-- A tábla indexei `rendelt_termek`
 --
 ALTER TABLE `rendelt_termek`
   ADD KEY `rendeles` (`rendeles_id`),
   ADD KEY `termekk` (`termek_id`);
 
 --
--- Indexes for table `termek`
+-- A tábla indexei `termek`
 --
 ALTER TABLE `termek`
   ADD PRIMARY KEY (`termek_id`);
 
 --
--- Indexes for table `vasarlo`
+-- A tábla indexei `vasarlo`
 --
 ALTER TABLE `vasarlo`
   ADD PRIMARY KEY (`vasarlo_id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- A kiírt táblák AUTO_INCREMENT értéke
 --
 
 --
--- AUTO_INCREMENT for table `beerkezesek`
+-- AUTO_INCREMENT a táblához `beerkezesek`
 --
 ALTER TABLE `beerkezesek`
   MODIFY `beerk_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `dolgozo`
+-- AUTO_INCREMENT a táblához `dolgozo`
 --
 ALTER TABLE `dolgozo`
-  MODIFY `dolgozo_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `dolgozo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `rendeles`
+-- AUTO_INCREMENT a táblához `rendeles`
 --
 ALTER TABLE `rendeles`
   MODIFY `rendeles_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `termek`
+-- AUTO_INCREMENT a táblához `termek`
 --
 ALTER TABLE `termek`
-  MODIFY `termek_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `termek_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
--- AUTO_INCREMENT for table `vasarlo`
+-- AUTO_INCREMENT a táblához `vasarlo`
 --
 ALTER TABLE `vasarlo`
   MODIFY `vasarlo_id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- Constraints for dumped tables
+-- Megkötések a kiírt táblákhoz
 --
 
 --
--- Constraints for table `beerkezesek`
+-- Megkötések a táblához `beerkezesek`
 --
 ALTER TABLE `beerkezesek`
   ADD CONSTRAINT `dolgozo` FOREIGN KEY (`dolgozo_id`) REFERENCES `dolgozo` (`dolgozo_id`);
 
 --
--- Constraints for table `beerkezett_termek`
+-- Megkötések a táblához `beerkezett_termek`
 --
 ALTER TABLE `beerkezett_termek`
   ADD CONSTRAINT `beerkezes` FOREIGN KEY (`beerk_id`) REFERENCES `beerkezesek` (`beerk_id`),
   ADD CONSTRAINT `termek` FOREIGN KEY (`termek_id`) REFERENCES `termek` (`termek_id`);
 
 --
--- Constraints for table `rendeles`
+-- Megkötések a táblához `rendeles`
 --
 ALTER TABLE `rendeles`
   ADD CONSTRAINT `vasarlo` FOREIGN KEY (`vasarlo_id`) REFERENCES `vasarlo` (`vasarlo_id`);
 
 --
--- Constraints for table `rendelt_termek`
+-- Megkötések a táblához `rendelt_termek`
 --
 ALTER TABLE `rendelt_termek`
   ADD CONSTRAINT `rendeles` FOREIGN KEY (`rendeles_id`) REFERENCES `rendeles` (`rendeles_id`),
